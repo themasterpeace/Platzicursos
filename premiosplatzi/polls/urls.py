1666dade87f1c5044e1f20aacd57555dafed43b0
Martin Esquite <mesquite275@gmail.com>
2022-05-27 17:54:00 -0600

    Creando vistas y empezando con templates

--- premiosplatzi/polls/urls.py
+++ premiosplatzi/polls/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import *
+from . import views
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="index"),
+    path("<int:question_id>/results/", views.results, name="index"),
+    path("<int:question_id>/vote/", views.vote, name="index")
+    
     
 ]
--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta no. {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta no. {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta no. {question_id}")
