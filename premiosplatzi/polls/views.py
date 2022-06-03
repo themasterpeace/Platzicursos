54fa8e4241f9be32176860a054b3d36f13037f01
Martin Esquite <mesquite275@gmail.com>
2022-06-03 17:40:51 -0600

    Guardando las modificaciones de las clases erro 404 creando formularios y modificando vista vote

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {"latest_question_list":latest_question_list})
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta no. {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta no. {question_id}")
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except:
     return HttpResponse(f"Estas votando a la pregunta no. {question_id}")
