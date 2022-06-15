245deb059d784558728dcd62d8bfcd4a953c1b5f
Martin Esquite <mesquite275@gmail.com>
2022-06-15 17:18:14 -0600

    creando primer test

--- premiosplatzi/polls/tests.py
+++ premiosplatzi/polls/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import datetime 
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+#Models 
+#Vistas 
+
+class QuestionModelTest(TestCase):
+    
+    def test_was_published_recently_with_future_question(self):
+        """was_published_recently returns False for question whose pud_date is in the future """
+        time = timezone.now() + datetime.delta(days=30)
+        future_question = Question(question_text="¿Quien es el mejor CD de Platzi?")
+        self.assertIs(future_question.was_published_recently(), False)
