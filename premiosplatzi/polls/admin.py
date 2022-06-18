8dcadfc8a2ef6a968f05ede1f47b08c05c811039
Martin Esquite <mesquite275@gmail.com>
2022-06-18 11:07:40 -0600

    Empezando con el frontend de premios platzi

--- premiosplatzi/polls/admin.py
+++ premiosplatzi/polls/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+    
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields=["question_text"]
+    
+    
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
